11e1a78b4e3e4ba10c877edfa41e496f8ac54269
Yibang Chen <ychen622@gmail.com>
2017-05-04 20:56:34 -0700

    update - final

--- get_rsa_key.py
+++ get_rsa_key.py
@@ -13,25 +13,6 @@ def gen_key(bits):
 	p = q = 3
 	while p == q:
 		p = get_randint()
-		p = random.randint(2**(bits/2-2), 2**(bits/2))
-		q = random.randint(2**(bits/2-2), 2**(bits/2))
-		p += not(p&1)	# changes the values from even to odd
-		q += not(q&1)
-
-		# while not is_prime(p):
-		# 	p -= 2
-		# while not is_prime(q):
-		# 	q -= 2
-	print type(p)        
-
-    n = p * q   
-    tot = (p-1) * (q-1)
-    e = tot
-    while gcd(tot,e) != 1:
-        e = random.randint(3,tot-1)
-    d = getd(tot,e)                       # gets the multiplicative inverse
-    while d<0:                            # i can probably replace this with mod
-        d = d + tot
-    return e,d,n
+		q = get_randint()
 
 gen_key(1024)
